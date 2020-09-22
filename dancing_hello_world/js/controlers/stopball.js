ce7445f8eff98d0003c10245c7afd877b5f39158
Javier Soler Artero <javier.soler.jsa@gmail.com>
2020-09-22 21:45:17 +0200

    Imports i exports DONE

--- dancing_hello_world/js/controlers/stopball.js
+++ dancing_hello_world/js/controlers/stopball.js
@@ -1,12 +1,9 @@
-//import {start} from "../main.js";
+'use strict'
+
+import {app} from "./../main.js";
 
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
-        if (stateApp=="run"){
-            stateApp="stop";
-            clearInterval(myApp);
-        }else{            
-            start();
-        }
+       app.toogle();
     }
 }
--- dancing_hello_world/js/main.js
+++ dancing_hello_world/js/main.js
@@ -1,35 +1,45 @@
-let myApp
-let stateApp="stop"
+import { docReady } from "./core/core";
 
-let inicia = function(el) {
-    // DOM is loaded and ready for manipulation here
+export let app = (function (el) {
+    let myApp
+    let stateApp = "stop"
+
     let speed = 15; //1 to 100
-    let incX = speed * (Math.round(Math.random())?1:-1);
-    let incY = speed * (Math.round(Math.random())?1:-1);
+    let incX = speed * (Math.round(Math.random()) ? 1 : -1);
+    let incY = speed * (Math.round(Math.random()) ? 1 : -1);
 
-    dw=function danceWorld(){
-    
-        x =  el.style.left?parseInt(el.style.left,10):350;
-        y =  el.style.top?parseInt(el.style.top,10):400;
+    let dw = function (el) {
 
-        el.style.left =  x + incX +"px";
-        el.style.top = y + incY+"px";
-    
+        x = el.style.left ? parseInt(el.style.left, 10) : 350;
+        y = el.style.top ? parseInt(el.style.top, 10) : 400;
+
+        el.style.left = x + incX + "px";
+        el.style.top = y + incY + "px";
+
         //Detect if we reach X coordinates limits
-        if (((x+incX) > (window.innerWidth-40)) || ((x+incX)<=0))
-            incX = (-1)*incX;
-       
+        if (((x + incX) > (window.innerWidth - 40)) || ((x + incX) <= 0))
+            incX = (-1) * incX;
+
         //Detect if we reach Y coordinates limits
-        if (((y+incY) > (window.innerHeight-40)) || ((y+incY) <= 0))
-            incY = (-1)*incY;
+        if (((y + incY) > (window.innerHeight - 40)) || ((y + incY) <= 0))
+            incY = (-1) * incY;
     }
-    //return dw;    
-};
 
-inicia(document.getElementById("ball"));
+    let start = function () {
+        stateApp = "run"
+        myApp = setInterval(dw(document.getElementById("ball")), 50);
+    }
 
-let start =function start(){
-    stateApp="run"
-    myApp=setInterval(dw,50);
-}
-docReady(start);
+    let stop = function () {
+        stateApp = "stop"
+        clearInterval(myApp)
+    }
+
+    let toogle = function () {
+        (stateApp==="run") ? stop() : start()
+    }
+
+    return {start: start, toogle: toogle}
+})();
+
+docReady(app.start);
