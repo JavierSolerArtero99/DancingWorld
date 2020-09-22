f06d341e9a649678aa2167f9ef5de2be2732d7c3
Javier Soler Artero <javier.soler.jsa@gmail.com>
2020-09-22 21:02:51 +0200

    Initial commit

--- dancing_hello_world/js/main.js
+++ dancing_hello_world/js/main.js
@@ -6,7 +6,6 @@ let inicia = function(el) {
     let speed = 15; //1 to 100
     let incX = speed * (Math.round(Math.random())?1:-1);
     let incY = speed * (Math.round(Math.random())?1:-1);
-    //let el=document.getElementById("el");
 
     dw=function danceWorld(){
     
